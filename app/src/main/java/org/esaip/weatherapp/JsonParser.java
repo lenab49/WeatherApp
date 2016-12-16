b3dd2eb9b347a56360b30445463ce8866c885cb5
lenaburon <lburon.ir2017@esaip.org>
2016-12-16 18:18:09 +0100

    add tache get temperature

---
+++ app/src/main/java/org/esaip/weatherapp/JsonParser.java
@@ -0,0 +1,56 @@
+package org.esaip.weatherapp;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by Léna on 16/12/2016.
+ */
+
+public class JsonParser {
+
+    public Weather parse(String feed) {
+        Weather weather = null;
+
+        try {
+            double latitude = 0.0;
+            double longitude = 0.0;
+            double currentTemp = 0.0;
+            double maxTemp = 0.0;
+            double minTemp = 0.0;
+            String description = null;
+
+            JSONObject root = new JSONObject(feed);
+
+            if(root.has("coord")) {
+                JSONObject coord = root.getJSONObject("coord");
+
+                longitude = coord.getDouble("lon");
+                latitude = coord.getDouble("lat");
+            }
+
+            if(root.has("weather")) {
+                JSONArray weatherArray = root.getJSONArray("weather");
+                JSONObject weatherObject = weatherArray.getJSONObject(0);
+                description = weatherObject.getString("description");
+            }
+
+            if(root.has("main")) {
+                JSONObject main = root.getJSONObject("main");
+
+                currentTemp = main.getDouble("temp");
+                maxTemp = main.getDouble("temp_min");
+                minTemp = main.getDouble("temp_max");
+            }
+
+
+            weather = new Weather(latitude, longitude, currentTemp, maxTemp, minTemp, description);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return weather;
+    }
+}
---
+++ app/src/main/java/org/esaip/weatherapp/Weather.java
@@ -0,0 +1,51 @@
+package org.esaip.weatherapp;
+
+/**
+ * Created by Léna on 16/12/2016.
+ */
+
+public class Weather {
+
+    private double latitude;
+    private double longitude;
+    private double currentTemp;
+    private double maxTemp;
+    private double minTemp;
+    private String description;
+
+    public Weather(double latitude, double longitude, double currentTemp,
+                   double maxTemp, double minTemp, String description) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.currentTemp = currentTemp;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
+        this.description = description;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public double getMinTemp() {
+        return minTemp;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getSummary() {
+        String info = "Weather Info:\nCurrent temp: " + currentTemp +
+                "°C\nCurrent weather: " + description;
+        return info;
+    }
+}
