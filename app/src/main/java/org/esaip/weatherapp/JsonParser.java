6a7ba213d91c3a92ed6b95840d5fd737d000fc38
lenaburon <lburon.ir2017@esaip.org>
2017-01-27 01:15:01 +0100

    Revert "erreur no value for dt_text"
    This reverts commit 6214f0595578ab8ce7e6c50f08950d5868d1f190.

--- app/src/main/java/org/esaip/weatherapp/JsonParser.java
+++ app/src/main/java/org/esaip/weatherapp/JsonParser.java
@@ -148,10 +148,10 @@ public class JsonParser {
                         Log.d(JsonParser.class.getSimpleName(),"max: "+ maxTemp);
                     }
                 }
-                if(list.has("dt_txt")){
-                    JSONObject listObject = list.getJSONObject("dt_txt");
+                if(list.has("dt_text")){
+                    JSONObject listObject = list.getJSONObject("dt_text");
                     date=listObject.getString("dt_text");
-                    Log.d(JsonParser.class.getSimpleName(),"dt_txt: "+ date);
+                    Log.d(JsonParser.class.getSimpleName(),"dt_text: "+ date);
                 }
             }
 
